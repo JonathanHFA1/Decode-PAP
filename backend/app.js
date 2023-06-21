e176a86730e427d5d25f58e92f105c385ae65e5f
JonathanHFA1 <93122368+JonathanHFA1@users.noreply.github.com>
2023-06-21 10:15:04 +0100

    primeiro commit

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
+require("dotenv").config();
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
+
+
+//Conexao a db
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+    useFindAndModify:false
+})
+.then(() => console.log("DB connected"))
+.catch((err)=> console.log(err));
+
+
+//MIDLEWARE
+app.use(morgan('dev'));
+app.use(bodyparser.json({limit:"5mb"}));
+app.use(bodyparser.urlencoded({
+    limit:"5mb",
+    extended:true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+//port
+const port = process.env.PORT || 9000
+
+app.listen(port, ()=> {
+    console.log(`servidor ta a dar na porta ${port}`);
+})
---
+++ backend/routes/authRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { sign } = require('jsonwebtoken');
+const router = express.Router();
+
+
+//auth routes
+
+// /api/signup
+router.post('/signup', signup);
+
+module.exports = router;
