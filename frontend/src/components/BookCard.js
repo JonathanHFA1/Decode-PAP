a882846fe8971b87dacc79d11401d1aedecd86b2
JonathanHFA1 <93122368+JonathanHFA1@users.noreply.github.com>
2023-07-03 17:46:59 +0100

    modal compra

--- frontend/src/components/BookCard.js
+++ frontend/src/components/BookCard.js
@@ -4,15 +4,11 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
+import ModalCompra from '../components/ModalCompra'
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import CommentIcon from '@mui/icons-material/Comment';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Box } from '@mui/system';
@@ -21,7 +17,13 @@ import { toast } from 'react-toastify';
 import { useState } from 'react';
 import Loader from './Loader';
 
+//modal
+
+
 const PostCard = ({ id, title, subheader, image, content, comments, likes, showPosts, likesId }) => {
+
+  const [openModal, setOpenModal] = useState(false)
+  
   const { userInfo } = useSelector((state) => state.signIn);
   const [loading, setLoading] = useState(false);
 
@@ -52,8 +54,12 @@ const PostCard = ({ id, title, subheader, image, content, comments, likes, showP
     }
   };
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345, bgcolor: '#252525' }}>
+      <div className="flex items-start justify-center text-white">
+        <p>Título do Livro</p>
+      </div>
       <CardHeader
+        className="text-white"
         title={title}
         // subheader={subheader}
       />
@@ -62,8 +68,8 @@ const PostCard = ({ id, title, subheader, image, content, comments, likes, showP
       </Link>
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          <p className="text-black">Descrição livro</p>
-          <Box component="span" dangerouslySetInnerHTML={{ __html: content.split(' ').slice(0, 10).join(' ') + '...' }}></Box>
+          <p className="text-white">Descrição livro</p>
+          <Box component="span" className="text-white" dangerouslySetInnerHTML={{ __html: content.split(' ').slice(0, 10).join(' ') + '...' }}></Box>
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -82,8 +88,44 @@ const PostCard = ({ id, title, subheader, image, content, comments, likes, showP
             )}
             {likes} Like(s)
           </Box>
+          <div>
+        <button onClick={() => setOpenModal(true)} className='bg-[#FF4E16]  hover:bg-orange-700 h-[43px] rounded-full py-2 px-4 font-bold text-white'>Comprar</button>
+      </div>
+      <ModalCompra isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)}>
+      <div className="z-50 px-4 py-6 text-left bg-[#252525] rounded-lg">
+              <h1 className="mb-4 text-2xl font-medium leading-6 text-white">
+                Coloque seus dados de compra
+              </h1>
+
+              <div className="space-y-4">
+                <input
+                  type="text"
+                  className="w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Nome"
+                />
+                <input
+                  type="text"
+                  className="w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Email"
+                />
+                <input
+                  type="text"
+                  className="w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Endereço"
+                />
+                <input
+                  type="text"
+                  className="w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Outra informação"
+                />
+              </div>
+            </div>
+      </ModalCompra>
+      
+
         </div>
       </CardActions>
+  
     </Card>
   );
 };
---
+++ frontend/src/components/ModalCompra.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const BACKGROUND_STYLE = {
+  backgroundColor: 'rgb(0,0,0, 0.7)',
+  zIndex: '1000',
+};
+
+const MODAL_STYLE = {
+  transform: 'translate(-50%,-50%)',
+};
+
+export default function Modal({ isOpen, setModalOpen, children }) {
+  if (isOpen) {
+    return (
+      <div style={BACKGROUND_STYLE} className="fixed top-0  bottom-0 left-0 right-0 p-[150px]  rounded-xl  text-black">
+        <div style={MODAL_STYLE} className="fixed top-[50%] left-[50%] p-[50px] bg-[#252525] rounded-xl text-black ">
+          <div className="flex flex-col items-center justify-end">
+            <div>{children}</div>
+            <button onClick={setModalOpen} className="bg-[#FF4E16] hover:bg-orange-700 h-[43px] rounded-full py-2 px-4 font-bold text-white">
+              Fechar
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+}
+
+// position: 'fixed',
+// top: 0,
+// left: 0,
+// right: 0,
+// bottom: 0,
+// backgroundColor: 'rgba(0, 0, 0, .7)',
+// zIndex: 1000
